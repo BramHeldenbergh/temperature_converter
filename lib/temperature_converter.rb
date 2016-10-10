eacf5a595389cb7d36de5083317677307d6c7b0f
bram <r0453918@student.vives.be>
2016-10-10 11:50:15 +0200

    single response class

--- lib/temperature_converter.rb
+++ lib/temperature_converter.rb
@@ -1,45 +1,51 @@
 
 class TemperatureConverter
+  KELVIN=273.15
+  attr_reader :temperature
 
-private
-def initialize temp
-  @temp = temp.to_f
-end
+  private
+  def initialize temperature
+    @temperature = temperature.to_f
+  end
 
-public
-def celsius
-  @temp
-end
+  public
+  def celsius
+    temperature
+  end
 
-def fahrenheit
-  @temp * 1.8 + 32
-end
+  def fahrenheit
+    temperature * 1.8 + plusfahrenheit
+  end
 
-def kelvin
-  @temp + 273.15
-end
+  def plusfahrenheit
+    32
+  end
 
-def to_text
-  %{
-    Temperature:
-    #{celsius} °C
-    #{fahrenheit} °F
-    #{kelvin} K
-  }
-end
+  def kelvin
+    temperature + KELVIN
+  end
 
-def to_json
-  %{{celsius: #{celsius}, Fahrenheit: #{fahrenheit}, Kelvin: #{kelvin}}
-  }
-end
+  def to_text
+    %{
+      Temperature:
+      #{celsius} °C
+      #{fahrenheit} °F
+      #{kelvin} K
+    }
+  end
 
-def to_html
-  %{<div>
-    <div> #{celsius} °C<div>
-    <div> #{fahrenheit} °F<div>
-    <div> #{kelvin} K<div>
-  <div>
-  }
-end
+  def to_json
+    %{{celsius: #{celsius}, Fahrenheit: #{fahrenheit}, Kelvin: #{kelvin}}
+    }
+  end
+
+  def to_html
+    %{<div>
+      <div> #{celsius} °C<div>
+      <div> #{fahrenheit} °F<div>
+      <div> #{kelvin} K<div>
+    <div>
+    }
+  end
 
 end
